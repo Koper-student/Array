79f28ab47c3dbb7d0a8e3e4464d7551c907452fd
Koper-student <237988@edu.p.lodz.pl>
2024-06-22 23:04:33 +0200

    Add menu elements

--- menu.cpp
+++ menu.cpp
@@ -9,9 +9,9 @@ void menu (void){
     printf("3. Display maximum value\n");
     printf("4. Display minimum value\n");
     printf("5. Display average value\n");
-    printf("6. Change size array\n");
-    printf("7. Change element in array\n");
-    printf("8. \n");
+    printf("6. Change element in array\n");
+    printf("7. Sort\n");
+    printf("8. Display median\n");
     printf("0. EXIT\n");
     printf("Select an option here: \n");
 }
